e77970e4a63cdffa10dd5a383a5f882ab42348e2
narayanasamy m <mech.narayanasamy@gmail.com>
2024-09-28 12:17:15 +0000

    3

--- test/components/navbar.tsx
+++ test/components/navbar.tsx
@@ -53,8 +53,7 @@ export const Navbar = () => {
       <NavbarContent className='basis-1/5 sm:basis-full' justify='start'>
         <NavbarBrand as='li' className='gap-3 max-w-fit'>
           <NextLink className='flex justify-start items-center gap-1' href='/'>
-            {/* <Logo /> */}
-            <p className='font-bold text-inherit'>Student Portal</p>
+            <p className='font-bold text-inherit'>STUDENT PORTAL</p>
           </NextLink>
         </NavbarBrand>
         <ul className='hidden lg:flex gap-4 justify-start ml-2'>
@@ -78,15 +77,44 @@ export const Navbar = () => {
       <NavbarContent
         className='hidden sm:flex basis-1/5 sm:basis-full'
         justify='end'
-      ></NavbarContent>
+      >
+        <NavbarItem className='hidden sm:flex gap-2'>
+          <ThemeSwitch />
+        </NavbarItem>
+        <NavbarItem className='hidden lg:flex'>{searchInput}</NavbarItem>
+        <NavbarItem className='hidden md:flex'></NavbarItem>
+      </NavbarContent>
 
       <NavbarContent className='sm:hidden basis-1 pl-4' justify='end'>
         <Link isExternal aria-label='Github' href={siteConfig.links.github}>
           <GithubIcon className='text-default-500' />
         </Link>
-
+        <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
+
+      <NavbarMenu>
+        {searchInput}
+        <div className='mx-4 mt-2 flex flex-col gap-2'>
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? 'primary'
+                    : index === siteConfig.navMenuItems.length - 1
+                      ? 'danger'
+                      : 'foreground'
+                }
+                href='#'
+                size='lg'
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
     </NextUINavbar>
   );
 };
